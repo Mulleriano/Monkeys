1a6c55d466422a3fd13e99d5dcbeab8991f283f7
Mulleriano <igormullersantos@hotmail.com>
2023-10-06 13:53:11 -0300

    Cria ViewModel da MainPage Cria método que recebe dados da API Inicia o Service criado para a MainPage

--- ViewModel/MonkeysViewModel.cs
+++ ViewModel/MonkeysViewModel.cs
@@ -1,6 +1,44 @@
-﻿namespace monkeys.ViewModel
+﻿using monkeys.Services;
+
+namespace monkeys.ViewModel
 {
-    public partial class MonkeysViewModel
+    public partial class MonkeysViewModel : BaseViewModel
     {
+        MonkeyService monkeyService;
+        public ObservableCollection<Monkey> Monkeys { get; } = new();
+
+        public MonkeysViewModel(MonkeyService monkeyService)
+        {
+            Title = "Monkeys";
+            this.monkeyService = monkeyService;
+        }
+
+        [RelayCommand]
+        async Task GetMonkeysAsync()
+        {
+            if (IsBusy)
+                return;
+
+            try
+            {
+                IsBusy = true;
+                var monkeys = await monkeyService.GetMonkeys();
+
+                if (Monkeys.Count != 0)
+                    Monkeys.Clear();
+
+                foreach (var monkey in monkeys)
+                    Monkeys.Add(monkey);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+                await Shell.Current.DisplayAlert("Error!", $"Unable to get Monkeys: {ex.Message}", "OK");
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+        }
     }
 }
